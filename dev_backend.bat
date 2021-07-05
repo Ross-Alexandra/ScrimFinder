cb004ac00f33114bb4d8ced63c796912462d9945
Ross Alexandra <Ross-Alexandra@outlook.com>
2021-07-05 14:30:07 -0700

    Removed files used for initial development
     - Ahead of a V0.1.0 release these files are no longer needed
       as:
        - The test features have been fully implemented, and
        - The app has been fully dockerized.

--- dev_backend.bat
+++
@@ -1,6 +0,0 @@
-start py -m scrim_finder.schedule_bot.bot
-start py -m scrim_finder.nasc_bot.bot
-
-set FLASK_APP=scrim_finder:app
-set FLASK_ENV=development
-flask run
--- dev_backend.sh
+++
@@ -1,6 +0,0 @@
-py -m scrim_finder.schedule_bot.bot
-py -m scrim_finder.nasc_bot.bot
-
-export FLASK_ENV=development
-export FLASK_APP=scrim_finder:app
-flask run
